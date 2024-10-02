79fc0f9bc9949179d7d5a2c877cb7278c4810665
Smriti <smriti12sci.nvm@gmail.com>
2024-10-02 11:50:51 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+dotenv.config({path: "./.env"});
+
+export const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Connected to database, DB HOST: ${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.error("ERROR: ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
